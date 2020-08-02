6085acb009c8f0225759567c797ac72960b6cf7f
Saptarshi Sarkar <67330269+saptarshisarkar20@users.noreply.github.com>
2020-08-02 19:01:20 +0530

    Add files via upload

---
+++ building Web Applications in PHP/week 5/assignment/index.php
@@ -0,0 +1,21 @@
+
+<title>"YOUR NAME"</title>
+<h1>"YOUR NAME"</h1>
+<p>
+   <?php
+	print "The SHA256 hash of \"YOUR NAME\" is ".hash('sha256', 'YOUR NAME');
+?>
+</p>
+<pre>ASCII ART:
+    **      **
+     **    **
+      **  **
+       ****
+        **
+        **
+        **
+        **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
